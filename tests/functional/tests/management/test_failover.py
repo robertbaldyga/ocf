d0fabaf21985e8551a5c323bf846ec6f8b8c608a
Adam Rutkowski <adam.j.rutkowski@intel.com>
2021-11-05 00:35:27 +0100

    failover test
    Signed-off-by: Adam Rutkowski <adam.j.rutkowski@intel.com>

--- tests/functional/tests/management/test_failover.py
+++ tests/functional/tests/management/test_failover.py
@@ -16,41 +16,49 @@ from pyocf.types.shared import (
     SeqCutOffPolicy,
 )
 from pyocf.utils import Size
-
-import pdb
+from pyocf.rio import Rio, ReadWrite
 
 def test_setup_failover(pyocf_2_ctx):
     ctx1 = pyocf_2_ctx[0]
     ctx2 = pyocf_2_ctx[1]
-    mode = CacheMode.WO
+    mode = CacheMode.WB
     cls = CacheLineSize.LINE_4KiB
 
-    prim_cache_backend_vol = RamVolume(Size.from_MiB(35))
-    prim_core_backend_vol = RamVolume(Size.from_MiB(100))
-    sec_cache_backend_vol = RamVolume(Size.from_MiB(35))
-    sec_core_backend_vol = RamVolume(Size.from_MiB(100))
+    prim_cache_backend_vol = RamVolume(Size.from_MiB(20))
+    core_backend_vol = RamVolume(Size.from_MiB(1))
+    sec_cache_backend_vol = RamVolume(Size.from_MiB(20))
 
-    # passive cache with core directly on ram disk
+    # passive cache with directly on ram disk
     cache2 = Cache(owner=ctx2, cache_mode=mode, cache_line_size=cls)
     cache2.start_cache()
     cache2.standby(sec_cache_backend_vol)
-
-    #core2 = Core.using_device(sec_core_backend_vol)
-    #cache2.add_core(core2)
 
     # volume replicating cache1 ramdisk writes to cache2 cache exported object
     cache2_exp_obj_vol = CacheVolume(cache2)
     cache1_cache_vol = ReplicatedVolume(prim_cache_backend_vol, cache2_exp_obj_vol)
 
     # active cache 
-    pdb.set_trace()
     cache1 = Cache.start_on_device(cache1_cache_vol, ctx1, cache_mode=mode, cache_line_size=cls)
-    core1 = Core.using_device(prim_core_backend_vol)
-    cache1.add_core(core1)
+    core = Core(core_backend_vol)
+    cache1.add_core(core)
 
+    # some I/O
+    r = Rio().target(core).njobs(1).readwrite(ReadWrite.WRITE).size(Size.from_MiB(1)).qd(1).run()
+
+    # capture checksum before simulated active host failure
+    md5 = core.exp_obj_md5()
+
+    # offline primary cache volume and stop primary cache to simulate active host
+    # failure
     cache1_cache_vol.offline()
     cache1.stop()
 
+    # failover 
     cache2.failover_detach()
-    cache2.activate(sec_cache_backend_vol)
+    cache2.activate(sec_cache_backend_vol, open_cores=False)
 
+    # add core explicitly with "try_add" to workaround pyocf limitations
+    core = Core(core_backend_vol)
+    cache2.add_core(core, try_add=True)
+    
+    assert md5 == core.exp_obj_md5()
